3e4d9cfab343a2332175d2cfacb77cba4661b515
LucasNobre <lnobre2003@gmail.com>
2024-12-16 00:07:37 -0300

    Fix: Missing key

--- src/app/commandments/page.tsx
+++ src/app/commandments/page.tsx
@@ -56,40 +56,43 @@ const CommandmentsList = () => {
                 </div>
             </div>
 
-
             <ul className="p-4 rounded-md">
                 {commandments.map((commandment) => (
-                    <div id="question-container" className="mb-6 bg-background-gray rounded-2xl border-b pb-4 shadow-lg">
-                        <li key={commandment.questionnaireNumber}>
+                    <div
+                        id="question-container"
+                        className="mb-6 bg-background-gray rounded-2xl border-b pb-4 shadow-lg"
+                        key={commandment.questionnaireNumber} // "key" adicionado aqui
+                    >
+                        <li>
                             <div id="title-head" className='bg-roman-red rounded-t-2xl p-2 text-center'>
-                            <h2 className="text-md font-semibold text-white">
-                                {commandment.questionnaireTitle}
-                            </h2>
-                            {commandment.questionnaireSubtitle && (
-                                <h1 className="text-lg text-white font-semibold oh1acity-70">{commandment.questionnaireSubtitle}</h1>
-                            )}
+                                <h2 className="text-md font-semibold text-white">
+                                    {commandment.questionnaireTitle}
+                                </h2>
+                                {commandment.questionnaireSubtitle && (
+                                    <h1 className="text-lg text-white font-semibold opacity-70">{commandment.questionnaireSubtitle}</h1>
+                                )}
                             </div>
                             
                             <ul className="m-4">
                                 {commandment.questions.map((question) => (
                                     <li key={question.questionNumber} className="mb-4">
-                                       <div id="question-title" className='flex items-center mb-2'>
-                                         <div id="number" className='bg-roman-red text-white font-semibold w-8 h-8 flex items-center justify-center rounded-md'>
-                                            {question.questionNumber}
-                                         </div>
-                                         <div id="title" className='ml-2 font-semibold text-lg'>
-                                            {question.questionTitle}
-                                         </div>
-                                       </div>
+                                        <div id="question-title" className='flex items-center mb-2'>
+                                            <div id="number" className='bg-roman-red text-white font-semibold w-8 h-8 flex items-center justify-center rounded-md'>
+                                                {question.questionNumber}
+                                            </div>
+                                            <div id="title" className='ml-2 font-semibold text-lg'>
+                                                {question.questionTitle}
+                                            </div>
+                                        </div>
                                         <ul className="ml-2">
                                             {question.options.map((option, index) => (
                                                 <li
-                                                    key={index}
+                                                    key={index} // "key" adicionado aqui
                                                     className={`pb-1 flex items-center space-x-2 ${option.disabled ? 'opacity-50' : ''}`}
                                                 >
                                                     <input
                                                         type="checkbox"
-                                                         className="w-5 h-5 appearance-none border-2 border-gray-400 rounded-md checked:bg-black checked:border-black"
+                                                        className="w-5 h-5 appearance-none border-2 border-gray-400 rounded-md checked:bg-black checked:border-black"
                                                         checked={option.checked || false}
                                                         disabled={option.disabled || false}
                                                         onChange={(e) =>
