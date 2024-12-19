4e91f69a8acf7cc1374aa1b7863da58ad1fb76f9
reispedroso <eduardopedroso3d2y@gmail.com>
2024-12-19 16:41:46 -0300

    fix

--- src/app/commandments/page.tsx
+++ src/app/commandments/page.tsx
@@ -1,7 +1,5 @@
 'use client';
-
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Commandment } from '@/repositories/interfaces/ICommandments';
 import { handleCheckboxChange } from '@/app/utils/handleCheckboxChange';
 import { fetchAndUpdateCommandments } from '../services/commandmentsService';
